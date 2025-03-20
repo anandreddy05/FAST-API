8926c89cf4febd9494ace41f83dfd0bf984523f0
Anand <anandreddy.s3215@gmail.com>
2025-03-20 21:30:03 +0530

    Added PostgreSQL

--- TODOS/database.py
+++ TODOS/database.py
@@ -3,9 +3,12 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args= {'check_same_thread':False})
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args= {'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
